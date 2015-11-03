b70990b331f17d20cd5378999c586bc40693d5f8
Atul Varma <varmaa@gmail.com>
2015-11-03 10:59:00 -0500

    better instructions for level five silver (aka level 6)

--- mini/levels/06.js
+++ mini/levels/06.js
@@ -1,13 +1,20 @@
 function setupLevel(){
+  // ***** LEVEL FIVE (SILVER) INSTRUCTIONS *****
+  //
+  // Update the number in the platformWidth VARIABLE to make all
+  // platforms have a width between 80 and 120.
+  //
+  // Update the color of the platformColor VARIABLE below to
+  // change all of the platform colors.
+  //
+  // ***** ENTER YOUR CODE BELOW *****
 
+  platformWidth = 40;
   platformColor = 'red';
 
-  //Update the number in this VARIABLE to make all platforms have a width between 80 and 120
-  platformWidth = 40;
-
-//Leave this code alone
-  createPlatform(90, 200, platformWidth, 10, platformColor);
-  createPlatform(190, 300, platformWidth, 10, platformColor);
-  createPlatform(290, 400, platformWidth, 10, platformColor);
-  createPlatform(390, 500, platformWidth, 10, platformColor);
+  // Leave this code alone
+  createPlatform(30, 100, platformWidth, 10, 'green');
+  createPlatform(190, 200, platformWidth, 10, platformColor);
+  createPlatform(290, 300, platformWidth, 10, platformColor);
+  createPlatform(390, 400, platformWidth, 10, platformColor);
 }
