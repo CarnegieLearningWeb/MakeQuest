edad2382aef83159488c5fb0e6e0bd96c67b632c
Atul Varma <varmaa@gmail.com>
2015-11-03 18:03:13 -0500

    update level configs for 9 and 10.

--- mini/levels/09.js
+++ mini/levels/09.js
@@ -1,7 +1,12 @@
 function setupLevel() {
+  // ***** LEVEL SIX (SILVER) INSTRUCTIONS *****
+  //
+  // Make two new platforms from scratch.
+  //
+  // Refer to the first line of code for help.
+  //
+  // ***** ENTER YOUR CODE BELOW *****
 
-  createPlatform(40, 100, 80, 20, “blue”);
-  
-  //Make two new platforms. Refer to the code above.
+  createPlatform(40, 100, 80, 20, 'blue');
 
 }
--- mini/levels/10.js
+++ mini/levels/10.js
@@ -1,11 +1,15 @@
 function setupLevel() {
+  // ***** LEVEL SIX (GOLD) INSTRUCTIONS *****
+  //
+  // Use the VARIABLES platformWidth, platformHeight and
+  // platformColor to make two new platforms from scratch.
+  //
+  // ***** ENTER YOUR CODE BELOW *****
+
   platformWidth = 80;
   platformHeight = 20;
   platformColor = "orange";
 
   createPlatform(40, 100, platformWidth, platformHeight, platformColor);
-  
-  //Use the VARIABLES platformWidth, platformHeight and platformColor to make two new platforms. Refer to the code above.
 
 }
-
