fd42eea21f770db4510a06414a3f2d18a2f564e1
Atul Varma <varmaa@gmail.com>
2015-11-03 10:41:27 -0500

    Embellish instructions for level 2.

--- mini/levels/02.js
+++ mini/levels/02.js
@@ -1,9 +1,25 @@
 function setupLevel() {
-//The first two numbers control the position, the last two numbers control the size.
-//Example (x-position, y-position, width, height);
-//INSTRUCTIONS:  Change the size of the platforms below so you can reach the goal
+  // ***** LEVEL TWO INSTRUCTIONS *****
+  //
+  // The first two numbers control the position, the last
+  // two numbers control the size.
+  //
+  // Example:
+  //
+  //   createPlatform(40, 100, 80, 10);
+  //                  ^   ^    ^   ^
+  //                  |   |    |   |
+  //                  |   |    |   height
+  //                  |   |    width
+  //                  |   y-position
+  //                  x-position
+  //
+  // TIP: The level has a WIDTH of 640 and a HEIGHT of 480
+  //
+  // Change the size of the platforms below so you can reach the goal.
+  //
+  // ***** ENTER YOUR CODE BELOW *****
 
-//TIP The level has a WIDTH of 640 and a HEIGHT of 480
   createPlatform(40, 100, 80, 10);
   createPlatform(60, 180, 80, 10);
   createPlatform(510, 400, 80, 10);
