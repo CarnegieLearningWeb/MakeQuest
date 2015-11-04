50dd11271ad44c34ba53b5acce7f2330acc806a9
Joan <joancc@gmail.com>
2015-11-04 17:17:26 -0500

    Fix speed

--- mini/levels/13.js
+++ mini/levels/13.js
@@ -1,9 +1,14 @@
 function setupLevel (){
 
+  MOVE_RIGHT = 0;
+  MOVE_LEFT = 180;
+  MOVE_DOWN = 90;
+  MOVE_UP = 270;
+
   platformWidth = 80;
   platformHeight = 10;
 
-  createPlatform(20, 150, platformWidth, platformHeight, 'orange');
+  createPlatform(20, 150, platformWidth, platformHeight, 'green');
   
   platform1 = createPlatform(20, 150, platformWidth, platformHeight, 'orange');
   speedPlatform1 = 0;
@@ -22,12 +27,12 @@ function drawLevel (){
   // Code inside drawLevel runs and runs and runs (runs eveyr frame?)
   // By updating the speed in here, you increase the speed so you can go faster and faster
   // 
-  // Update the value of speedPlatform1 to increase by 0.05 units instead of 0.01
+  // Update the value of speedPlatform1 to increase by 0.005 units instead of 0.001
   // 
   // ***** ENTER YOUR CODE BELOW *****
-  speedPlatform1 = speedPlatform1;
+  speedPlatform1 = speedPlatform1 + 0.001;
 
-  platform1.setSpeed( speedPlatform1, 0.01 );
+  platform1.setSpeed( speedPlatform1, MOVE_RIGHT );
   
   
 
