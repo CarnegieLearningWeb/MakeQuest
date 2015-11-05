81ba677cdd2e0be390b91f3030cb74622d786c2c
Joan <Joan@Jikas-MacBook-Pro.local>
2015-11-05 18:12:31 -0500

    Fix level 4

--- mini/levels/04.js
+++ mini/levels/04.js
@@ -1,4 +1,5 @@
 function setupLevel(){ 
+  // EDITOR: beginReadOnly();
   // ***** LEVEL FOUR INSTRUCTIONS *****
   //
   // Change the word 'red' to something else
@@ -11,15 +12,20 @@ function setupLevel(){
   // ***** ENTER YOUR CODE BELOW *****
 
   createPlatform(40, 100, 80, 10, 'green');
+  // EDITOR: markHint('createPlatform', '(', null, ',', null, ',', null, ',', null, ',', {type: 'String', highlight: true});
+  // EDITOR: endReadOnly();
   createPlatform(160, 200, 80, 10, 'red');
   createPlatform(250, 300, 80, 10, 'red');
   createPlatform(400, 370, 80, 10, 'green');
   createPlatform(510, 400, 80, 10, 'red');
 
-
- // ***** CHALLENGE ********
- //Challenge1 (bronze level) -- Change each platform to a  *different* color.
- //Challenge 2 (silver level) -- Change each platform to a *square* instead of a rectangle. See comment on this slide for code.
+  // EDITOR: beginReadOnly();
+  // EDITOR: beginCodeFold('Click here for a CHALLENGE.');
+  // ***** CHALLENGE ********
+  //Challenge1 (bronze level) -- Change each platform to a  *different* color.
+  //Challenge 2 (silver level) -- Change each platform to a *square* instead of a rectangle.
+  // EDITOR: endCodeFold('Click here for a CHALLENGE.');
+  // EDITOR: endReadOnly();
 
 }
 
