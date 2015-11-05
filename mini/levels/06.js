b76ccd38cdfc7e7cd90b181c7f518b510c3b1dd4
Joan <Joan@Jikas-MacBook-Pro.local>
2015-11-05 18:37:47 -0500

    Fix 6

--- mini/levels/06.js
+++ mini/levels/06.js
@@ -1,4 +1,5 @@
 function setupLevel(){
+  // EDITOR: beginReadOnly();
   // ***** LEVEL FIVE (SILVER) INSTRUCTIONS *****
   //
   // Update the number in the platformWidth VARIABLE to change
@@ -8,15 +9,16 @@ function setupLevel(){
   // change all of the platform colors.
   //
   // ***** ENTER YOUR CODE BELOW *****
-
+  // EDITOR: endReadOnly();
+  // EDITOR: markHint('platformWidth', '=', {type: 'Numeric', highlight: true});
+  // EDITOR: markHint('platformColor', '=', {type: 'String', highlight: true});
   platformWidth = 40;
   platformColor = 'red';
 
   // EDITOR: beginReadOnly();
-  // Leave this code alone
   createPlatform(30, 100, platformWidth, 10, 'green');
+  // EDITOR: endReadOnly();
   createPlatform(190, 200, platformWidth, 10, platformColor);
   createPlatform(290, 300, platformWidth, 10, platformColor);
   createPlatform(390, 400, platformWidth, 10, platformColor);
-  // EDITOR: endReadOnly();
 }
