b3682e5e280f7e630fc9432ca80c085b7dc991a7
Joan <joancc@gmail.com>
2015-11-06 10:01:05 -0500

    Fix jump levels bronze and silver

--- mini/levels/14.js
+++ mini/levels/14.js
@@ -1,14 +1,27 @@
 function drawLevel() {
-    if ( isPlayerOnPlatform() ) {
+  // EDITOR: beginReadOnly();
+  if ( isPlayerOnPlatform() ) {
+  // EDITOR: endReadOnly();
+  
+  // ***** LEVEL EIGHT (BRONZE) INSTRUCTIONS *****
+  // 
+  // Use a number different from 0 to give the jump some force.
+  // 
+  // ***** ENTER YOUR CODE BELOW *****
+  
+    makePlayerJump( 0 );
+  
+  // EDITOR: beginReadOnly();
+  }
+  // EDITOR: endReadOnly();
 
-    // ***** LEVEL EIGHT (BRONZE) INSTRUCTIONS *****
-    // 
-    // Use a number different from 0 to give the jump some force.
-    // 
-    // ***** ENTER YOUR CODE BELOW *****
-    
-      makePlayerJump( 0 );
 
-    }
-
+  // EDITOR: beginCodeFold('Click here for an EXAMPLE.');
+  // ******See Example Below******:
+  //
+  //   makePlayerJump( 5 );
+  //                   ^
+  //                   |
+  //                   force
+  // EDITOR: endCodeFold();
 }
--- mini/levels/15.js
+++ mini/levels/15.js
@@ -1,14 +1,23 @@
 function drawLevel() {
-    if ( isPlayerOnPlatform() ) {
-
-    // ***** LEVEL EIGHT (SILVER) INSTRUCTIONS *****
-    // 
-    // Use the right amount of force to reach the goal with a single jump.
-    // 
-    // ***** ENTER YOUR CODE BELOW *****
-    
-      makePlayerJump( 0 );
-
-    }
-
+  // EDITOR: beginReadOnly();
+  if ( isPlayerOnPlatform() ) {
+  // EDITOR: endReadOnly();
+  // ***** LEVEL EIGHT (SILVER) INSTRUCTIONS *****
+  // 
+  // Use the right amount of force to reach the goal with a single jump.
+  // 
+  // ***** ENTER YOUR CODE BELOW *****
+  
+    makePlayerJump( 0 );
+  // EDITOR: beginReadOnly();
+  }
+  // EDITOR: endReadOnly();
+  // EDITOR: beginCodeFold('Click here for an EXAMPLE.');
+  // ******See Example Below******:
+  //
+  //   makePlayerJump( 5 );
+  //                   ^
+  //                   |
+  //                   force
+  // EDITOR: endCodeFold();
 }
