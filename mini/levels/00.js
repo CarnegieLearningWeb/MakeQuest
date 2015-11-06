dcfe99ee3243b1661be01d5233f2e7fafa62b3da
Joan <joancc@gmail.com>
2015-11-06 16:23:13 -0500

    Show code for level 0

---
+++ mini/levels/00.js
@@ -0,0 +1,28 @@
+function setupLevel() {
+  // EDITOR: beginReadOnly();
+  platformHeight = 10;
+
+  playerImage = loadImage("images/heroD.png");
+  player.addImage(playerImage);
+
+  createPlatform(180, 320, 80, platformHeight, 'blue');
+  createPlatform(150, 460, 130, platformHeight, 'magenta');
+  createPlatform(350, 460, 20, platformHeight, 'magenta');
+  createPlatform(450, 460, 20, platformHeight, 'magenta');
+  createPlatform(500, 390, 40, platformHeight, 'pink');
+  createPlatform(600, 320, 40, platformHeight, 'pink');
+  createPlatform(350, 260, 80, platformHeight, 'orange');
+  createPlatform(30, 400, 80, platformHeight, 'brown');
+  
+  goal.position.x = 500;
+  goal.position.y = 150;
+  goalImage = loadImage("images/star.png");
+  goal.addImage(goalImage);
+}
+
+function drawLevel() {
+  if (isPlayerOnPlatform()) {
+    makePlayerJump(4);
+  }
+// EDITOR: endReadOnly();
+}
