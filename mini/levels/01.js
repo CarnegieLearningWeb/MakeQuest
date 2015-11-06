e0c1ebb3a959d05b87c4514d03d89f48f9bdd1a3
Joan <joancc@gmail.com>
2015-11-06 15:47:07 -0500

    Reminder to click on the game to interact

--- mini/levels/01.js
+++ mini/levels/01.js
@@ -1,2 +1,6 @@
-//Use the arrow keys to move left and right. 
-//Try to get your character from one side of the level to the secret passage.
+// Use the arrow keys to move left and right. 
+// Try to get your character from one side of the level to the secret passage.
+
+
+// TIP: If nothing happens when you press the arrow keys, 
+// try ‘clicking’ with your mouse on the game screen (on the right) first
