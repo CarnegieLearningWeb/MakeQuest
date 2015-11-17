79fbf4b49c07e5b17d0ce86f959ce4eba46fbac0
Joan <joancc@gmail.com>
2015-11-17 13:48:18 -0500

    Refactor error display

--- js/custom-errors.js
+++ js/custom-errors.js
@@ -1,18 +1,19 @@
 var CustomErrors = {
+  //Main funciton to be called when performing tests
   test: function(){
     var code = editor_js.getValue();
     tokens = esprima.tokenize( code, {range: true, loc: true} );
 
+    // Find all tokens for which we have a custom error checking function
     for (var i = 0; i < tokens.length; i++) {
       
+      // Each custom function should handle errors on its own.
       if( tokens[i].type == "Identifier" && tokens[i].value == "createPlatform" ){
         var err = CustomErrors.createPlatform(tokens, i);
         if( err ){
-          
-          markJsErrorAtLine( err.token.loc.start.line );
-
-          $('#errorModal p.error-text').text( err.errMsg );
-          $('#errorModal').foundation('reveal', 'open');
+          //Break as soon as we find an error to provide errors one at a time
+          return true;  
+         
         }
       }
 
@@ -24,13 +25,13 @@ var CustomErrors = {
                 // We might allow multiple options, such as Identifier or Numeric. Use arrays to account for that
                  {type: ['Punctuator'],           value: ['('],  errorMsg: ''  },
                  {type: ['Numeric'],              value: null ,  errorMsg: ''  },
-                 {type: ['Punctuator'],           value: [','],  errorMsg: ''  },
+                 {type: ['Punctuator'],           value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
                  {type: ['Numeric'],              value: null ,  errorMsg: ''  },
-                 {type: ['Punctuator'],           value: [','],  errorMsg: ''  },
+                 {type: ['Punctuator'],           value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
                  {type: ['Numeric'],              value: null ,  errorMsg: ''  },
-                 {type: ['Punctuator'],           value: [','],  errorMsg: ''  },
+                 {type: ['Punctuator'],           value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
                  {type: ['Numeric'],              value: null ,  errorMsg: ''  },
-                 {type: ['Punctuator'],           value: [','],  errorMsg: ''  },
+                 {type: ['Punctuator'],           value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
                  {type: ['String', 'Identifier'], value: null ,  errorMsg: CustomErrors.errorMsgs.color },
                  {type: ['Punctuator'],           value: [')'],  errorMsg: ''  },
                  ];
@@ -43,8 +44,16 @@ var CustomErrors = {
       var tokenValue = tokens[tokenIndex].value;
 
       if( tests[j].type.indexOf( tokenType ) == -1 ){
-        console.log( tokenIndex )
-        return {token: tokens[tokenIndex], errMsg: tests[j].errorMsg}
+        // Error found. Display to the user and exit
+
+        var err = {
+          token: tokens[tokenIndex], 
+          errMsg: tests[j].errorMsg
+        };
+
+        this.displayError( err );
+        
+        return true;
       }
 
       // If we need a specific value, check it
@@ -54,8 +63,15 @@ var CustomErrors = {
     };           
     
   },
+  displayError: function(err){
+    markJsErrorAtLine( err.token.loc.start.line );
+
+    $('#errorModal p.error-text').text( err.errMsg );
+    $('#errorModal').foundation('reveal', 'open');
+  },
   errorMsgs: {
-    color: "You need a valid color name. Make sure you're spelling the name of your color correctly, and that it's surrounded with quotation marks."
+    color: "You need a valid color name. Make sure you're spelling the name of your color correctly, and that it's surrounded with quotation marks.",
+    comma: "Every value or ARGUMENT inside parentheses must be separated by a comma"
   }
 }
 
