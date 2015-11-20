3632747f31dbcbe685b071f2113ffc283fb047b6
Joan <joancc@gmail.com>
2015-11-20 11:06:16 -0500

    Display readonly in italics. Extend CustomErrors handling of createPlatform.

--- js/custom-errors.js
+++ js/custom-errors.js
@@ -33,42 +33,64 @@ var CustomErrors = {
     color: "You need a valid color name. Make sure you're spelling the name of your color correctly, and that it's surrounded with quotation marks.",
     comma: "Every value or ARGUMENT inside parentheses must be separated by a comma",
     semicolon: "Every line of code or STATEMENT must end with a semicolon ';' ",
+    parentheses: "You need an opening '(' and a closing ')' parentheses.",
   },
 
   // Custom functions to handle tailored feedback.
   createPlatform: function(tokens, index){
-    var tests = [
-                // We might allow multiple options, such as Identifier or Numeric. Use arrays to account for that
-                 {type: ['Punctuator'],            value: ['('],  errorMsg: ''  },
-                 {type: ['Numeric'],               value: null ,  errorMsg: ''  },
-                 {type: ['Punctuator'],            value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
-                 {type: ['Numeric'],               value: null ,  errorMsg: ''  },
-                 {type: ['Punctuator'],            value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
-                 {type: ['Numeric', 'Identifier'], value: null ,  errorMsg: ''  },
-                 {type: ['Punctuator'],            value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
-                 {type: ['Numeric', 'Identifier'], value: null ,  errorMsg: ''  },
-                 {type: ['Punctuator'],            value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
-                 {type: ['String', 'Identifier'],  value: null ,  errorMsg: CustomErrors.errorMsgs.color },
-                 {type: ['Punctuator'],            value: [')'],  errorMsg: ''  },
-                 {type: ['Punctuator'],            value: [';'],  errorMsg: CustomErrors.errorMsgs.semicolon  },
-                 ];
+    // Handle multilpe signatures of createPlatform (4 and 5 arguments)
+    var tests = {
+        fourArgs: [
+           // We might allow multiple options, such as Identifier or Numeric. Use arrays to account for that
+           {type: ['Punctuator'],            value: ['('],  errorMsg: CustomErrors.errorMsgs.parentheses  },
+           {type: ['Numeric'],               value: null ,  errorMsg: ''  },
+           {type: ['Punctuator'],            value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
+           {type: ['Numeric'],               value: null ,  errorMsg: ''  },
+           {type: ['Punctuator'],            value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
+           {type: ['Numeric', 'Identifier'], value: null ,  errorMsg: ''  },
+           {type: ['Punctuator'],            value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
+           {type: ['Numeric', 'Identifier'], value: null ,  errorMsg: ''  },
+           {type: ['Punctuator'],            value: [')'],  errorMsg: CustomErrors.errorMsgs.parentheses  },
+           {type: ['Punctuator'],            value: [';'],  errorMsg: CustomErrors.errorMsgs.semicolon  },
+         ],
+         fiveArgs: [
+           // We might allow multiple options, such as Identifier or Numeric. Use arrays to account for that
+           {type: ['Punctuator'],            value: ['('],  errorMsg: CustomErrors.errorMsgs.parentheses  },
+           {type: ['Numeric'],               value: null ,  errorMsg: ''  },
+           {type: ['Punctuator'],            value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
+           {type: ['Numeric'],               value: null ,  errorMsg: ''  },
+           {type: ['Punctuator'],            value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
+           {type: ['Numeric', 'Identifier'], value: null ,  errorMsg: ''  },
+           {type: ['Punctuator'],            value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
+           {type: ['Numeric', 'Identifier'], value: null ,  errorMsg: ''  },
+           {type: ['Punctuator'],            value: [','],  errorMsg: CustomErrors.errorMsgs.comma  },
+           {type: ['String', 'Identifier'],  value: null ,  errorMsg: CustomErrors.errorMsgs.color },
+           {type: ['Punctuator'],            value: [')'],  errorMsg: CustomErrors.errorMsgs.parentheses  },
+           {type: ['Punctuator'],            value: [';'],  errorMsg: CustomErrors.errorMsgs.semicolon  },
+         ]
+    }
+
+    var test = ( currentLevel < 4 ) ? tests.fourArgs : tests.fiveArgs;
+
   
-    for (var j = 0; j < tests.length; j++) {
+    for (var j = 0; j < test.length; j++) {
       // Need to add 1 to the passed index to begin checks after the triggering identifier
       var tokenIndex = index + 1 + j;
 
       var tokenType = tokens[tokenIndex].type;
       var tokenValue = tokens[tokenIndex].value;
 
-      if( tests[j].type.indexOf( tokenType ) == -1 ){
+      if( test[j].type.indexOf( tokenType ) == -1 ){
+        console.log("Err A");
+        console.log(tokenType);
+        console.log(test[j]);
+        console.log(test);
+        console.log(tokens[tokenIndex]);
         // Error found. Display to the user and exit
-        
-
-
         var err = {
           // Use original index to highlight the correct line
           token: tokens[index], 
-          errMsg: tests[j].errorMsg
+          errMsg: test[j].errorMsg
         };
 
         this.displayError( err );
@@ -77,15 +99,13 @@ var CustomErrors = {
       }
 
       // If we need a specific value, check it
-      if( tests[j].value && tests[j].value.indexOf( tokenValue ) == -1 ){
+      if( test[j].value && test[j].value.indexOf( tokenValue ) == -1 ){
+        console.log("Err B");
         // Error found. Display to the user and exit
-
-        console.log(tokens[tokenIndex]);
-
         var err = {
           // Use original index to highlight the correct line
           token: tokens[index], 
-          errMsg: tests[j].errorMsg
+          errMsg: test[j].errorMsg
         };
 
         this.displayError( err );
