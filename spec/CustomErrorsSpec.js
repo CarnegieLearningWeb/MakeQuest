b153a80d772b30e47a7ffadb699f7d97e4583940
Joan <joancc@gmail.com>
2015-11-24 13:11:48 -0500

    Add makePlayerJump test

--- spec/CustomErrorsSpec.js
+++ spec/CustomErrorsSpec.js
@@ -1,38 +1,44 @@
 // Globals for tests
 var currentLevel;
 
-describe('CustomErrors', function(){
-  it('should have custom error messages', function(){
-    expect(CustomErrors.errorMsgs).toEqual(jasmine.anything());
-  });
+describe('CustomErrors', function() {
+    it('should have custom error messages', function() {
+        expect(CustomErrors.errorMsgs).toEqual(jasmine.anything());
+    });
 
-  describe('CustomErrors methods', function(){
-    it('should have a method to test the syntax for a call to createPlatform', function(){
+    describe('createPlatform', function() {
+        it('should return an err if the method call does not end with a semicolon', function() {
+            var code = "createPlatform(40, 100, 80, 10),";
+            currentLevel = 3;
+            var err = CustomErrors.test(code);
+            expect(err).toEqual(jasmine.anything());
+            expect(err.errMsg).toEqual(CustomErrors.errorMsgs.semicolon);
+        });
 
-      expect(CustomErrors.createPlatform).toEqual(jasmine.anything());
+        it('should return an err if the arguments are not separated by commas', function() {
+            var code = "createPlatform(40, 100, 80 10);";
+            currentLevel = 3;
+            var err = CustomErrors.test(code);
+            expect(err).toEqual(jasmine.anything());
+            expect(err.errMsg).toEqual(CustomErrors.errorMsgs.comma);
+        });
+    });
 
-      describe('createPlatform', function(){
-        it('should return an err if the method call does not end with a semicolon', function(){
-          var code = "createPlatform(40, 100, 80, 10),";
+    describe('makePlayerJump', function() {
+      it('should return an err if the method call does not end with a semicolon', function() {
+          var code = "makePlayerJump(5),";
+          var err = CustomErrors.test(code);
+
+          expect(err).toEqual(jasmine.anything());
+          expect(err.errMsg).toEqual(CustomErrors.errorMsgs.semicolon);
+      });
+      it('should return an err if the number of arguments is not correct', function() {
+          var code = "makePlayerJump(40, 40);";
           currentLevel = 3;
           var err = CustomErrors.test(code);
 
           expect(err).toEqual(jasmine.anything());
-          expect(err.errMsg).toEqual( CustomErrors.errorMsgs.semicolon );
-        });
-
-        it('should return an err if the arguments are not separated by commas', function(){
-          var code = "createPlatform(40, 100, 80 10);";
-          currentLevel = 3;
-          var err = CustomErrors.test(code);
-
-          expect(err).toEqual(jasmine.anything());
-          expect(err.errMsg).toEqual( CustomErrors.errorMsgs.comma );
-        });
       });
     });
 
-    it('should have a method to test the syntax for a call to makePlayerJump', function(){
-    });
-  });
 });
