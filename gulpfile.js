811c8c076d8505e9e062f7aa2e90e86e73ae2b69
Joan <joancc@gmail.com>
2015-12-08 09:44:12 -0500

    Remove unused files

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gulp = require('gulp');
+var UglifyJS = require('uglify-js');
+var map = require('vinyl-map');
+var concat = require('gulp-concat');
+
+gulp.task('uglify' , function uglifyTask() {
+
+  var uglify = map(function (buff, filename) {
+    var u = UglifyJS.minify(filename, {
+      // specify `UglifyJS` options here
+    });
+    return u.code;
+  });
+
+  return gulp.src('src/**/*.js')
+    .pipe(uglify)
+    .pipe(concat('uglified.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('uglify-vendor' , function uglifyTask() {
+
+  var uglify = map(function (buff, filename) {
+    var u = UglifyJS.minify(filename, {
+      // specify `UglifyJS` options here
+    });
+    return u.code;
+  });
+
+  return gulp.src('vendor/**/*.js')
+    .pipe(uglify)
+    .pipe(concat('uglified.js'))
+    .pipe(gulp.dest('dist/vendor'));
+});
+
+gulp.task('default', ['uglify']);
