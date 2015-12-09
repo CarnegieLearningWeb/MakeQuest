fbf1618e64035ca86ad801ab6b8f8713d5e05ce5
Joan <joancc@gmail.com>
2015-12-09 15:34:58 -0500

    Minification only

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,21 @@ var minifyCss = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('compress', function() {
+    //TODO: Serving compressed files from S3. (We're only minifying for now)
+    //To use gzip with S3 we have to intercept calls and change the url depending on the content-header
+    //Sample full compression with gzip:
+    //gulp.src('./js/**/*.js', {base: './'})
+    //.pipe(sourcemaps.init())
+    //.pipe(uglify())
+    //.pipe(sourcemaps.write())
+    //.pipe(gzip())
+    //.pipe(gulp.dest('./dist'));
+
+    /**** APP JS *****/    
     gulp.src('./js/**/*.js', {base: './'})
     .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(sourcemaps.write())
-    .pipe(gzip({ append: false }))
     .pipe(gulp.dest('./dist'));
 
     /**** CSS *****/    
@@ -17,31 +27,26 @@ gulp.task('compress', function() {
     .pipe(sourcemaps.init())
     .pipe(minifyCss())
     .pipe(sourcemaps.write())
-    .pipe(gzip({ append: false }))
     .pipe(gulp.dest('./dist'));
 
     /**** Vendor fodler *****/
     gulp.src('./vendor/**/*.js', {base: './'})
     .pipe(uglify())
-    .pipe(gzip({ append: false }))
     .pipe(gulp.dest('./dist'));
 
     /**** Compress bower_components *****/
     // Foundation
     gulp.src('./bower_components/foundation/js/foundation.min.js', {base: './'})
     .pipe(uglify())
-    .pipe(gzip({ append: false }))
     .pipe(gulp.dest('./dist'));
 
     // jQuery
     gulp.src('./bower_components/jquery/dist/jquery.min.js', {base: './'})
     .pipe(uglify())
-    .pipe(gzip({ append: false }))
     .pipe(gulp.dest('./dist'));
 
     // Modernizr
     gulp.src('./bower_components/modernizr/modernizr.js', {base: './'})
     .pipe(uglify())
-    .pipe(gzip({ append: false }))
     .pipe(gulp.dest('./dist'));
 });
