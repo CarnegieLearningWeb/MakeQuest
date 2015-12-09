c1b8e6684a0d9b89be0740a350631dd9f1a6f449
Joan <joancc@gmail.com>
2015-12-09 13:10:38 -0500

    Remove extra comment

--- mini/levels/09.js
+++ mini/levels/09.js
@@ -10,8 +10,8 @@ function setupLevel() {
   // The line of code below creates the platform you are standing on
   createPlatform(40, 100, 80, 20, 'blue');
   // EDITOR: endReadOnly();
-  // 
   // Use the previous line of code as reference to create your own
+  
   
   // EDITOR: beginCodeFold('Click here for an example.');
   // ******See Example Below******:
